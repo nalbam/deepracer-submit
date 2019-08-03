279c091f6f39da6c5f5adddb896b5c7ceea8edae
nalbam <me@nalbam.com>
2019-08-03 19:46:31 +0900

    all

--- submit.sh
+++ submit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
 SHELL_DIR=$(dirname $0)
 
 pushd ${SHELL_DIR}
@@ -14,9 +16,37 @@ if [ -f config/deepracer-model.sh ]; then
 fi
 
 if [ "${MODEL_URL}" != "" ]; then
-    export MODEL="$(curl -sL ${MODEL_URL} | xargs)"
+    # export MODEL="$(curl -sL ${MODEL_URL} | xargs)"
+
+    MODELS=config/models.txt
+
+    curl -sL ${MODEL_URL} > ${MODELS}
+
+    COUNT=$(cat ${MODELS} | wc -l | xargs)
+
+    echo "COUNT: ${COUNT}"
+
+    # random
+    if [ "${COUNT}" -gt "1" ]; then
+        if [ "${OS_NAME}" == "darwin" ]; then
+            RND=$(ruby -e "p rand(1...${COUNT})")
+        else
+            RND=$(shuf -i 1-${COUNT} -n 1)
+        fi
+    else
+        RND=1
+    fi
+
+    echo "RND: ${RND}"
+
+    # get one
+    if [ ! -z ${RND} ]; then
+        export MODEL=$(sed -n ${RND}p ${MODELS})
+    fi
 fi
 
-python3 submit.py
+echo "MODEL: ${MODEL}"
+
+# python3 submit.py
 
 popd
