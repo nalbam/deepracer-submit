24cba762b362538cce151661a2cf352acb75cd4e
nalbam <me@nalbam.com>
2020-03-13 12:25:21 +0900

    shm

--- submit.py
+++ submit.py
@@ -25,8 +25,8 @@ def open_browser():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--single-process")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--single-process")
     options.add_argument("--disable-dev-shm-usage")
 
     browser = webdriver.Chrome(options=options)
