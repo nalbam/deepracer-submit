85e3ba69e1283e707f4f709b592887d7366b87ab
nalbam <me@nalbam.com>
2020-03-13 12:13:56 +0900

    sandbox

--- submit.py
+++ submit.py
@@ -25,7 +25,7 @@ def open_browser():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    # options.add_argument('--no-sandbox')
+    options.add_argument("--no-sandbox")
     # options.add_argument('--single-process')
     # options.add_argument('--disable-dev-shm-usage')
 
