5488158c1a98488b7c5191a6629276c03f32b229
nalbam <me@nalbam.com>
2020-03-18 01:57:00 +0900

    find_element_by_id

--- submit.py
+++ submit.py
@@ -131,22 +131,24 @@ def submit_model(args, browser):
 
         browser.find_element_by_class_name("awsui-dropdown-trigger").click()
 
-        for data in browser.find_elements_by_xpath(
-            "//*[contains(@data-value)]/@data-value"
-        ):
-            print(data.text)
+        browser.find_element_by_id("awsui-select-0-dropdown-option-0").click()
 
-            arn = "arn:aws:deepracer:us-east-1:{}:model/reinforcement_learning/{}".format(
-                args.userno, args.model
-            )
+        # for data in browser.find_elements_by_xpath(
+        #     "//*[contains(@data-value)]/@data-value"
+        # ):
+        #     print(data.text)
 
-            if data == arn:
-                data.click()
+        #     arn = "arn:aws:deepracer:us-east-1:{}:model/reinforcement_learning/{}".format(
+        #         args.userno, args.model
+        #     )
 
-        # element = browser.find_element_by_class_name("awsui-select-trigger-label")
-        # browser.execute_script(
-        #     'arguments[0].innerHTML = "{}";'.format(args.model), element
-        # )
+        #     if data == arn:
+        #         data.click()
+
+        element = browser.find_element_by_class_name("awsui-select-trigger-label")
+        browser.execute_script(
+            'arguments[0].innerHTML = "{}";'.format(args.model), element
+        )
 
         browser.find_element_by_class_name("awsui-button-variant-primary").click()
 
