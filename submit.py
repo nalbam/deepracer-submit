35566928fb8a4125b0f24be08953b6343f4c4303
nalbam <me@nalbam.com>
2021-04-20 02:47:58 +0900

    set_window_size(1600, 1700)

--- submit.py
+++ submit.py
@@ -51,7 +51,7 @@ def open_browser(args):
 
     # browser = webdriver.Chrome("/usr/local/bin/chromedriver")
 
-    browser.set_window_size(1600, 1800)
+    browser.set_window_size(1600, 1700)
 
     return browser
 
