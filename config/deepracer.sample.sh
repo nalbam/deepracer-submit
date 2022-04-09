fabfbbefc3a79deaaa4b3a69fce993fd70df2fd5
nalbam <me@nalbam.com>
2022-04-09 10:24:32 +0900

    sample config

---
+++ config/deepracer.sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export USERNO="" # 123456789012
+export USERNAME="username"
+export PASSWORD="password"
+
+export TARGET_URL="https://nalbam.com/deepracer/submit.json"
+
+export SLACK_TOKEN="" # xoxb-1111-2222-xxxx
+export SLACK_CHANNAL="#sandbox"
