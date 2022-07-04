d23bf6df560f71fec721d56d5b20e4a046417ac0
nalbam <me@nalbam.com>
2022-07-04 15:31:55 +0900

    globalNav-search

--- submit.py
+++ submit.py
@@ -133,10 +133,15 @@ def submit_model(doc, args, browser):
         try:
             browser.find_element(By.XPATH, "//body").click()
 
-            # awsui_dismiss-control_1d2i7_1qdlw_312 awsui_button_vjswe_1asap_101 awsui_variant-modal-dismiss_vjswe_1asap_162
+            # globalNav-search-0419 globalNav-search-0425
             browser.find_element(
-                By.CSS_SELECTOR, "button[class*='awsui_variant-modal']"
+                By.CSS_SELECTOR, "input[class*='globalNav-search']"
             ).click()
+
+            # # awsui_dismiss-control_1d2i7_1qdlw_312 awsui_button_vjswe_1asap_101 awsui_variant-modal-dismiss_vjswe_1asap_162
+            # browser.find_element(
+            #     By.CSS_SELECTOR, "button[class*='awsui_variant-modal']"
+            # ).click()
 
             # # awsui_button_vjswe_1asap_101 awsui_variant-primary_vjswe_1asap_206
             # browser.find_element(
