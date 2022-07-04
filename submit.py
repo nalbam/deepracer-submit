edc07e627c6e9fba2095984450bec12fb35ac76b
nalbam <me@nalbam.com>
2022-07-04 15:18:50 +0900

    html click

--- submit.py
+++ submit.py
@@ -131,6 +131,7 @@ def submit_model(doc, args, browser):
         time.sleep(5)
 
         try:
+            browser.find_element(By.XPATH, "//html").click()
             browser.find_element(By.XPATH, "//body").click()
 
             # # awsui_dismiss-control_1d2i7_1qdlw_312 awsui_button_vjswe_1asap_101 awsui_variant-modal-dismiss_vjswe_1asap_162
